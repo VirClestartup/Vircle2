b250cea68568e1677c23cd8aa368cece1e2fae7c
Vionitahosana <vionitahosana1203@gmail.com>
2022-11-21 17:59:34 +0800

    nuat komunitas update

--- app/src/main/java/id/kharisma/studio/vircle/BuatKomunitas.kt
+++ app/src/main/java/id/kharisma/studio/vircle/BuatKomunitas.kt
@@ -3,7 +3,7 @@ package id.kharisma.studio.vircle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class BuatKomunitasActivity : AppCompatActivity() {
+class BuatKomunitas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buat_komunitas)
