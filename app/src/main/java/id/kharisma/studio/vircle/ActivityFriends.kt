ad0c33e993f2ff4af9757c1afe923ed0726ba5d0
Vionitahosana <vionitahosana1203@gmail.com>
2022-11-21 17:46:18 +0800

    update friends

---
+++ app/src/main/java/id/kharisma/studio/vircle/ActivityFriends.kt
@@ -0,0 +1,11 @@
+package id.kharisma.studio.vircle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityFriends : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friends)
+    }
+}
