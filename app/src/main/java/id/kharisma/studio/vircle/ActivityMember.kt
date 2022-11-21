087b30b3fa3a03e68ac1df1bbbc89ca5a934394e
Vionitahosana <vionitahosana1203@gmail.com>
2022-11-21 18:09:56 +0800

    member

---
+++ app/src/main/java/id/kharisma/studio/vircle/ActivityMember.kt
@@ -0,0 +1,11 @@
+package id.kharisma.studio.vircle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityMember : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_member)
+    }
+}
