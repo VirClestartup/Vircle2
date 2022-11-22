4b70a85bbda6686f27f27af62d5c19a3be812e50
XYGALAB <96215279+XYGALAB@users.noreply.github.com>
2022-11-23 00:08:33 +0800

    Update buat komunitas, dan membership

--- app/src/main/java/id/kharisma/studio/vircle/ActivityMembership.kt
+++ app/src/main/java/id/kharisma/studio/vircle/ActivityMembership.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class ActivityMembership : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_membership)
+        setContentView(R.layout.activity_member)
     }
 }
